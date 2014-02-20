ec5187a09d1e588cad0731729c501bf19332bab5
Liron Yahdav <lyahdav@gmail.com>
2014-02-20 08:36:31 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Liron Yahdav on 2/20/14.
+//  Copyright (c) 2014 Liron Yahdav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
